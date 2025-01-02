201593d86adb9984c4ba08a047999ce6b9e7037e
mlodic <mlodic@users.noreply.github.com>
2025-01-02 16:41:45 +0000

    Deployed caf3960 with MkDocs version: 1.6.0

---
+++ Submodules/IntelOwl/api_app/analyzers_manager/queryset.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING, Type
+
+from django.db.models import QuerySet
+
+from api_app.queryset import AbstractReportQuerySet
+
+if TYPE_CHECKING:
+    from api_app.analyzers_manager.serializers import AnalyzerReportBISerializer
+
+
+class AnalyzerReportQuerySet(AbstractReportQuerySet):
+    @classmethod
+    def _get_bi_serializer_class(cls) -> Type["AnalyzerReportBISerializer"]:
+        from api_app.analyzers_manager.serializers import AnalyzerReportBISerializer
+
+        return AnalyzerReportBISerializer
+
+    def get_data_models(self, job) -> QuerySet:
+        DataModel = self.model.get_data_model_class(job)  # noqa
+        return DataModel.objects.filter(
+            pk__in=self.values_list("data_model_object_id", flat=True)
+        )
