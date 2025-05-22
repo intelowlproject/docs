3790012ee588004e219bc8e1f118882b0384b741
fgibertoni <fgibertoni@users.noreply.github.com>
2025-05-22 06:36:25 +0000

    Deployed 47158c3 with MkDocs version: 1.6.0

---
+++ Submodules/GreedyBear/greedybear/tasks.py
@@ -0,0 +1,79 @@
+# This file is a part of GreedyBear https://github.com/honeynet/GreedyBear
+# See the file 'LICENSE' for copying permission.
+from __future__ import absolute_import, unicode_literals
+
+from celery import chain, shared_task
+from greedybear.settings import CLUSTER_COWRIE_COMMAND_SEQUENCES
+
+
+@shared_task()
+def extract_log4pot():
+    from greedybear.cronjobs.log4pot import ExtractLog4Pot
+
+    ExtractLog4Pot().execute()
+
+
+@shared_task()
+def extract_cowrie():
+    from greedybear.cronjobs.cowrie import ExtractCowrie
+
+    ExtractCowrie().execute()
+
+
+# FEEDS
+@shared_task()
+def extract_general():
+    from greedybear.cronjobs.general import ExtractAllGenerals
+
+    ExtractAllGenerals().execute()
+
+
+@shared_task()
+def extract_sensors():
+    from greedybear.cronjobs.sensors import ExtractSensors
+
+    ExtractSensors().execute()
+
+
+@shared_task()
+def monitor_honeypots():
+    from greedybear.cronjobs.monitor_honeypots import MonitorHoneypots
+
+    MonitorHoneypots().execute()
+
+
+@shared_task()
+def monitor_logs():
+    from greedybear.cronjobs.monitor_logs import MonitorLogs
+
+    MonitorLogs().execute()
+
+
+# SCORING
+@shared_task()
+def chain_train_and_update():
+    from greedybear.cronjobs.scoring.scoring_jobs import TrainModels, UpdateScores
+
+    trainer = TrainModels()
+    trainer.execute()
+
+    updater = UpdateScores()
+    updater.data = trainer.current_data
+    updater.execute()
+
+
+# COMMANDS
+@shared_task()
+def cluster_commands():
+    from greedybear.cronjobs.commands.cluster import ClusterCommandSequences
+
+    if CLUSTER_COWRIE_COMMAND_SEQUENCES:
+        ClusterCommandSequences().execute()
+
+
+# CLEAN UP
+@shared_task()
+def clean_up_db():
+    from greedybear.cronjobs.cleanup import CleanUp
+
+    CleanUp().execute()
---
+++ Submodules/IntelOwl/api_app/analyzers_manager/file_analyzers/blint_scan.py
@@ -0,0 +1,39 @@
+import logging
+import os
+import shutil
+
+from blint.config import BlintOptions
+from blint.lib.runners import AnalysisRunner
+from django.conf import settings
+
+from api_app.analyzers_manager.classes import FileAnalyzer
+from intel_owl.settings._util import set_permissions
+
+logger = logging.getLogger(__name__)
+
+
+class BlintAnalyzer(FileAnalyzer):
+    """
+    Wrapper for Blint static analysis tool
+    """
+
+    def update(self) -> bool:
+        pass
+
+    def run(self) -> dict:
+        logger.info(f"Running Blint on {self.filepath} for {self.md5}")
+
+        reports_dir = settings.BLINT_REPORTS_PATH / f"blint_analysis_{self.md5}"
+        os.mkdir(reports_dir)
+        set_permissions(reports_dir)
+
+        analyzer = AnalysisRunner()
+        findings, reviews, fuzzables = analyzer.start(
+            blint_options=BlintOptions(reports_dir=reports_dir),
+            exe_files=[self.filepath],
+        )
+        response = {"findings": findings, "reviews": reviews, "fuzzables": fuzzables}
+
+        shutil.rmtree(reports_dir)
+
+        return response
